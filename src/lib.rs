176e3ccba3df732a3941ad615cff298f556fea61
khammersmith17 <kilianhammersmith@gmail.com>
2025-07-12 12:58:18 -0400

    adding some tests, assertion was right in the first place

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ where
     T: Default + Clone,
 {
     pub fn new(cap: usize) -> CircularBuffer<T> {
-        assert!(cap <= 0_usize, "Attempt to initialize 0 size buffer");
+        assert!(cap > 0_usize, "Attempt to initialize 0 size buffer");
         let mut buf: Vec<T> = Vec::with_capacity(cap);
         buf.resize(cap, T::default());
         CircularBuffer {
@@ -39,15 +39,14 @@ where
         (self.tail + self.len + 1) % self.len
     }
 
-    pub fn peek_from_end(&self, length_from_end: usize) -> Option<&T> {
-        if self.len == 1 {
-            return None;
-        }
-
-        let i = if self.tail == 0 {
-            self.cap - 1
+    pub fn peek_from_end(&self, len_from_tail: usize) -> Option<&T> {
+        let i = if len_from_tail > self.tail {
+            if self.len != self.cap {
+                return None;
+            }
+            self.cap - (len_from_tail - self.tail)
         } else {
-            self.tail - 1
+            self.tail - len_from_tail
         };
 
         Some(&self.buf[i])
@@ -153,5 +152,30 @@ mod tests {
         // tail is index 1
         // head should be tail + 1 when it is wrapped
         assert_eq!(head, 0_usize);
+    }
+
+    #[test]
+    fn test_peek_from_end() {
+        let mut buffer: CircularBuffer<i32> = CircularBuffer::new(10_usize);
+        buffer.insert(1_i32);
+        assert_eq!(buffer.peek_from_end(3_usize), None);
+
+        buffer.insert(2_i32);
+        buffer.insert(3_i32);
+        buffer.insert(4_i32);
+
+        assert_eq!(buffer.peek_from_end(2_usize), Some(&2_i32));
+
+        buffer.insert(5_i32);
+        buffer.insert(6_i32);
+        buffer.insert(7_i32);
+        buffer.insert(8_i32);
+        buffer.insert(9_i32);
+        buffer.insert(10_i32);
+        buffer.insert(11_i32);
+        buffer.insert(12_i32);
+        buffer.insert(13_i32);
+        assert_eq!(buffer.peek_from_end(2_usize), Some(&11_i32));
+        assert_eq!(buffer.peek_from_end(5_usize), Some(&8_i32));
     }
 }
